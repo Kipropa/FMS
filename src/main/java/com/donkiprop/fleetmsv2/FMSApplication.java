9878950cbb27e80b63de3bb00e7d27318295ea2c
Kiprop <kipropa77@gmail.com>
2023-01-17 12:03:27 +0300

    First commit

---
+++ src/main/java/com/donkiprop/fleetmsv2/FMSApplication.java
@@ -0,0 +1,13 @@
+package com.donkiprop.fleetmsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FMSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donkiprop/fleetmsv2/CoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donkiprop.fleetmsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
